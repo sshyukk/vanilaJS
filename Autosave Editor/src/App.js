86857a66884a2ec99802982bc6ab66d1bef7582c
sshyukk <sanghyuk0328@gmail.com>
2021-09-07 16:44:07 +0900

    feat: 에디터 자동 저장기능 완성

--- Autosave Editor/src/App.js
+++ Autosave Editor/src/App.js
@@ -11,21 +11,23 @@ import PostEditPage from "./PostEditPage.js"
 
 
 export default function App({ $target }) {
-
-
     const postsPage = new PostsPage({ $target })
     const postEditPage = new PostEditPage( { $target, initialState: {
-        postId: 'new'
+        postId: 'new',
+        post: {
+            title: '',
+            content: '',
+        }
     } })
 
-    // 라우터 처리
+    // 라우팅 처리
     this.route = () => {
         const { pathname } = window.location
 
         if (pathname === '/') {
             postsPage.render()
         } else if (pathname.indexOf('/posts/') === 0) {
-            const [ , , postId] = pathname.split('/')
+            const [, , postId] = pathname.split('/')
             postEditPage.setState({ postId })
         }
     }
--- Autosave Editor/src/PostEditPage.js
+++ Autosave Editor/src/PostEditPage.js
@@ -1,7 +1,7 @@
 // 여기서 초기값은 편집할 id값을 넘겨받는다. 어느 페이지를 편집하고 있는지 알아보기 위함!
 import { request } from './api.js'
 import Editor from './Editor.js'
-import { getItem, setItem } from './storage.js'
+import { getItem, removeItem, setItem } from './storage.js'
 
 export default function PostEditPage({ $target, initialState }) {
     const $postEditPage = document.createElement('div')
@@ -10,10 +10,10 @@ export default function PostEditPage({ $target, initialState }) {
 
     // [editor 가지고 오기]
     // 로컬 스토리지에 값이 이미 있는 경우 초기 값 세팅. 키 값.
-    const TEMP_POST_SAVE_KEY = 'temp-post-${this.state.postId}'
+    let postLocalSaveKey = 'temp-post-${this.state.postId}'
 
     // 새로 작성하는 페이지가 아니면 원래 있던 페이지를 불러온다. 포스트 불러오기.
-    const post = getItem(TEMP_POST_SAVE_KEY, {
+    const post = getItem(postLocalSaveKey, {
         title: '',
         content: ''
     })
@@ -22,25 +22,46 @@ export default function PostEditPage({ $target, initialState }) {
     // 일단 PostsPage.js에 넣기 전에 main.js에서 시험을 해보자.[완료]
     const editor = new Editor({ 
         $target: $postEditPage,
-        initialState: this.state.post, // [?]그냥 post로 해야하지 않을까?
+        initialState: {
+            title: '',
+            content: '',
+        }, // [?]그냥 post로 해야하지 않을까?
         onEditing: (post) => {
             // debounce 처리.
             if (timer !== null) {
                 clearTimeout(timer)
             }
             // setTimeout 일정한 숫자값 반환
-            timer = setTimeout(() => {
-                setItem(TEMP_POST_SAVE_KEY, {
+            timer = setTimeout(async () => {
+                setItem(postLocalSaveKey, {
                     ...post,
                     // 저장시간 측정.
                     tempSaveDate: new Date()
                 })
-            }, 1000)
+                const isNew = this.state.postId === 'new'
+                if (isNew) {
+                    const createdPost = await request('/posts', {
+                        method: 'POST',
+                        body: JSON.stringify(post)
+                    })
+                    history.replaceState(null, null, `/posts/${createdPost.id}`)
+                    removeItem(postLocalSaveKey)
+                } else {
+                    await request(`/posts/${post.id}`, {
+                        method: 'PUT',
+                        body: JSON.stringify(post)
+                    })
+                    removeItem(postLocalSaveKey)
+
+                }
+            }, 2000)
         },
     })
 
     this.setState = async nextState => {
         if (this.state.postId !== nextState.postId) {
+            postLocalSaveKey = 'temp-post-${nextState.postId}'
+
             this.state = nextState
             await fetchPost()
             return 
@@ -49,7 +70,10 @@ export default function PostEditPage({ $target, initialState }) {
         this.state = nextState
         this.render()
 
-        editor.setState(this.state.post)
+        editor.setState(this.state.post || {
+            title: '',
+            content: ''
+        })
     }
 
     this.render = () => {
@@ -63,6 +87,22 @@ export default function PostEditPage({ $target, initialState }) {
         // 선택적 불러오기. 아이디 키 값이 있으면 수정. 아니면 새로 생성.
         if (postId !== 'new') {
             const post = await request(`/posts/${postId}`)
+
+            // 새로고침 시 psot 내용 업데이트 하기.
+            const tempPost = getItem(postLocalSaveKey, {
+                title: '',
+                content: ''
+            })
+
+            if (tempPost.tempSaveDate && tempPost.tempSaveDate > post.updated_at) {
+                if ( confirm('저장되지 않은 임시 데이터가 있습니다. 불러올까요?')) {
+                    this.setState({
+                        ...this.state,
+                        post: tempPost
+                    })
+                    return 
+                }
+            }
 
             this.setState({
                 ...this.state,
