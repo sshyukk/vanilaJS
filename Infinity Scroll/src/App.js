26dc4884144142c157af46a34140ea9d773b360f
sshyukk <sanghyuk0328@gmail.com>
2021-09-08 18:58:36 +0900

    feat: 스크롤 추가버튼 기능 예외처리 추가

--- Infinity Scroll/src/App.js
+++ Infinity Scroll/src/App.js
@@ -3,37 +3,50 @@ import { request } from './api.js'
 
 
 export default function App({ $target }) {
+    // 헤더 제목 이름 생성
     const $h1 = document.createElement('h1')
     $h1.innerText = 'Cat Photos'
     $h1.style.textAlign = 'center'
     $target.appendChild($h1)
-
+    // 기본 값 설정
     this.state = {
         limit: 5,
         nextStart: 0, // limit 갯수만큼 계속 더해짐
-        photos: []
+        photos: [],
+        isLoading: false
     }
+    // 고양이 사진 리스트 생성
     const photoListComponent = new PhotoList({
         $target,
-        initialState: this.state.photos,
+        initialState: {
+            isLoading: this.state.isLoading,
+            photos: this.state.photos
+        },
         onScrollEnded: async () => {
             await fetchPhotos()
         }
     })
-
+    // 상태 재설정
     this.setState = nextState => {
         this.state = nextState
-        photoListComponent.setState(nextState.photos)
+        photoListComponent.setState({
+            isLoading: this.state.isLoading,
+            photos: nextState.photos
+        })
     }
-
+    // 사진 api 불러오기
     const fetchPhotos = async () => {
+        this.setState({
+            ...this.state,
+            isLoading: true
+        })
         const { limit, nextStart } = this.state
-
         const photos = await request(`/cat-photos?_limit=${limit}&_start=${nextStart}`)
         this.setState({
             ...this.state,
             nextStart: nextStart + limit,
-            photos: [ ...this.state.photos, ...photos ]
+            photos: [ ...this.state.photos, ...photos ],
+            isLoading: false,
         })
     }
     fetchPhotos()
--- Infinity Scroll/src/PhotoList.js
+++ Infinity Scroll/src/PhotoList.js
@@ -7,6 +7,8 @@ initialState = [
 ]
 */
 export default function PhotoList({ $target, initialState, onScrollEnded }) {
+    // 기본요소
+    let isInitialize = false
     const $photoList = document.createElement('div')
     $target.appendChild($photoList)
     this.state = initialState
@@ -14,26 +16,34 @@ export default function PhotoList({ $target, initialState, onScrollEnded }) {
         this.state = nextState
         this.render()
     }
-
+    //렌더링
     this.render = () => {
-        console.log(this.state)
-        $photoList.innerHTML = `
-        <ul>
-            ${this.state.map(photo => 
-                `
-                    <li style="list-style: none;">
-                        <img width="100%" src="${photo.imagePath}" />
-                    </li>
-                `
-            ).join('')}
-        </ul>
-        <button class="PhotoList__loadMore" style="width: 100%; height: 200px; font-size: 20px;">Load More</button>
-        `
+        // 렌더링 리스트 생성 중복 방지
+        if (!isInitialize) {
+            $photoList.innerHTML = `
+                <ul class="PhotoList__photos"></ul>
+                <button class="PhotoList__loadMore" style="width: 100%; height: 200px; font-size: 20px;">Load More</button>
+            `
+            isInitialize = true
+        }
+        const { photos } = this.state
+        const $photos = $photoList.querySelector('.PhotoList__photos')
+        photos.forEach(photo => {
+            // photo의 id 기준으로 렌더링이 되어 있는지 체크
+            if ($photos.querySelector(`li[data-id="${photo.id}"]`) === null) {
+                // 없으면 li 생성하고 $photos에 appendChild
+                const $li = document.createElement('li')
+                $li.setAttribute('data-id', photo.id)
+                $li.style = 'list-style: none;'
+                $li.innerHTML = `<img width="100%" src="${photo.imagePath}" />`
+                $photos.appendChild($li)
+            }
+        })
     }
     this.render()
-
+    // 사진 불러오기 버튼 생성
     $photoList.addEventListener('click', e => {
-        if (e.target.className === 'PhotoList__loadMore') {
+        if (e.target.className === 'PhotoList__loadMore' && !this.state.isLoading) {
             onScrollEnded()
         }
     })
