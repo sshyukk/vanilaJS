47467afbe9fd695a984ee62f16442699360b0b15
(sshyukk) <(sanghyuk0328@gmail.com)>
2021-09-08 16:15:45 +0900

    feat: 더미 데이터 불러오기 구현

---
+++ Infinity Scroll/src/PhotoList.js
@@ -0,0 +1,32 @@
+/*
+initialState = [
+    {
+        id: 1,
+        imagePath: ''
+    }
+]
+*/
+
+export default function PhotoList({ $target, initialState, onScrollEnded }) {
+    const $photoList = document.createElement('ul')
+    $target.appendChild($photoList)
+    this.state = initialState
+    this.setState = nextState => {
+        this.state = nextState
+        this.render()
+    }
+
+    this.render = () => {
+        console.log(this.state)
+        $photoList.innerHTML = `
+            ${this.state.map(photo => 
+                `
+                    <li>
+                        <img width="100%" src="${photo.imagePath}" />
+                    </li>
+                `
+            ).join('')}
+        `
+    }
+    this.render()
+}
